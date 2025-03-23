d5f17e1e521634b536000eae1673d55f959d6313
aayushi-saxena-me <rashmi.srivastava@zocdoc.com>
2025-03-23 11:45:12 -0400

    cleanup

--- OpenAISample.py
+++ OpenAISample.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-  api_key="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"
+  api_key=""
 )
 
 completion = client.chat.completions.create(
